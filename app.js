1123df06470ea2ff19c7cdfb5436fb8f1a1dceae
Alejandro Jr Cartojano (TCI) <v-alejca@microsoft.com>
2018-12-18 09:04:47 +0800

    first

---
+++ app.js
@@ -0,0 +1,65 @@
+
+var sortByTitle = document.getElementById('sort-by-title'),
+    sortByAuthor = document.getElementById('sort-by-author'),
+    sortByRating = document.getElementById('sort-by-rating'),
+    sortBydate = document.getElementById('sort-by-date'),
+    multiAuthor = document.getElementById('multi-author'),
+    doc = document.getElementById('books');
+
+
+const books = data;
+
+function displayBooks(books){
+    doc.innerHTML = "";
+    if(books.length > -1){
+        for(var i = 0; i < books.length; i++){
+            if(books[i].rating == ''){
+                books[i].rating = "No rating yet";
+            }
+            var html =
+            "<div class='book-block'>\
+                <p><img src='" + books[i].image + "'></p>\
+                <div class='details'>\
+                    <h3>" + books[i].title + "</h3>\
+                    <p>Book by: " + books[i].author + "</p>\
+                    <p>Rating: " + books[i].rating + "</p>\
+                    <p>Published: " + books[i].releaseDate + "</p>\
+                </div>\
+            </div>";
+
+            doc.innerHTML += html;
+        }
+    } else {
+        doc.innerHTML = "No matches!";
+    }
+}
+
+
+sortByTitle.addEventListener('click', function(){
+    var sortedData = books.sort((a, b) => a.title > b.title ?  1 : -1);
+    displayBooks(sortedData);
+});
+
+sortByAuthor.addEventListener('click', function(){
+    var sortedData = books.sort((a, b) => a.author > b.author ?  1 : -1);
+    displayBooks(sortedData);
+});
+
+sortByRating.addEventListener('click', function(){
+    var sortedData = books.sort((a, b) => a.rating < b.rating ?  1 : -1);
+    displayBooks(sortedData);
+});
+
+sortBydate.addEventListener('click', function(){
+    var sortedData = books.sort((a, b) => a.releaseDate > b.releaseDate ?  1 : -1);
+    displayBooks(sortedData);
+});
+
+multiAuthor.addEventListener('click', function(){
+    var sortedData = books.filter(a => a.author.indexOf(',') != -1);
+
+    console.log(sortedData)
+    displayBooks(sortedData);
+});
+
+displayBooks(books);
---
+++ js/data.js
@@ -0,0 +1,80 @@
+var data = [
+    {
+        title : 'Maus',
+        author : 'Art Spiegelman',
+        rating : 4.5,
+        releaseDate: 1980,
+        image : 'data/images/maus.jpg'
+    },
+    {
+        title : 'From Hell',
+        author : 'Alan Moore',
+        rating : 4.2,
+        releaseDate : 1989,
+        image : 'data/images/from-hell.jpg'
+    },
+    {
+        title : 'Green Lantern Corps',
+        author : 'John Broome, Gil Kane',
+        rating : 4.1,
+        releaseDate : 2006,
+        image : 'data/images/green-lantern-corps.jpg'
+    },
+    {
+        title : 'Batman: The Killing Joke',
+        author : 'Alan Moore',
+        rating : 4.4,
+        releaseDate : 1988,
+        image : 'data/images/batman-the-killing-joke.jpg'
+    },
+    {
+        title : 'Arkham Asylum: A Serious House on Serious Earth',
+        author : 'Grant Morrison',
+        rating : 4.1,
+        releaseDate : 1989,
+        image : 'data/images/arkham-asylum.jpg'
+    },
+    {
+        title : 'X-Men: Days of Future Past',
+        author : 'Chris Claremont',
+        rating : 4.5,
+        releaseDate : 1981,
+        image : 'data/images/x-men-days-of-future-past.jpg'
+    },
+    {
+        title : 'Clueless: One Last Summer',
+        author : 'Amber Benson, Sarah Kuhn',
+        rating : '',
+        releaseDate : 2018,
+        image : 'data/images/clueless-one-last-summer.jpg'
+    },
+    {
+        title : 'Saga',
+        author : 'Brian K. Vaughan',
+        rating : 4.4,
+        releaseDate : 2012,
+        image : 'data/images/from-hell.jpg'
+    },
+    {
+        title : 'Alack Sinner',
+        author : 'Carlos Sampayo, José Antonio Muñoz',
+        rating : 3.8,
+        releaseDate : 2007,
+        image : 'data/images/alack-sinner.jpg'
+    },
+    {
+        title : 'The Trigan Empire',
+        author : 'Mike Butterworth',
+        rating : 4,
+        releaseDate : 2017,
+        image : 'data/images/the-trigan-empire.jpg'
+    },
+    {
+        title : 'The Bronze Age',
+        author : 'Bob Haney, Julius Schwartz, Paul Levitz, Robert Kanigher',
+        rating : '',
+        releaseDate : 2017,
+        image : 'data/images/the-bronze-age.jpg'
+    }
+    
+]
